44d078670c319c0ed2292c99f3ba710eff65ef5e
Kaspar Matas <kasparmat@hotmail.com>
2020-10-31 16:43:35 +0000

    Add ESLint dependency
    Added linter package dependency and fixed shown warnings

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const path = require('path');
-const Database = require('better-sqlite3')
-const db = new Database('my_data.db')
+const Database = require('better-sqlite3');
+const db = new Database('my_data.db');
 
 const { CommandoClient, SyncSQLiteProvider } = require('discord.js-commando');
-const { Discord } = require('discord.js');
 
 require('dotenv').config();
 const {
     PREFIX,
     OWNERS,
-    TOKEN
-} = process.env
+    TOKEN,
+} = process.env;
 
 const client = new CommandoClient({
     commandPrefix: PREFIX,
@@ -31,7 +30,7 @@ client.registry
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    console.log(`Available guilds:`);
+    console.log('Available guilds:');
     client.guilds.cache.each(guild => {
         console.log(`${guild.name}(${guild.id})`);
     });
